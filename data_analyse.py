5579f3cb53e8eb19565af75f836b7467b1259f3f
Siemon <alexandre.lambert@edu.ece.fr>
2020-11-24 10:13:08 +0100

    move video to frame in mai folder

--- data_analyse.py
+++ data_analyse.py
@@ -5,7 +5,12 @@ import pandas as pd
 landmarks_eyes_left = np.arange(36,42)
 landmarks_eyes_rigth = np.arange(42,48)
 
-df = pd.read_csv("data.csv")
+
+def compute_ear():
+
+
+
+df = pd.read_csv("data/landmarks.csv")
 
 df["ear_l"] = (df["euclid_dist_38_42_l"]+df["euclid_dist_39_41_l"])/(2*df["euclid_dist_37_40_l"])
 df["ear_r"] = (df["euclid_dist_44_48_r"]+df["euclid_dist_45_47_r"])/(2*df["euclid_dist_43_46_r"])
