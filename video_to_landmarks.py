b94e71810acf7ef4bab34d281c447412bf490c3c
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2021-01-15 12:42:57 +0100

    cahge print in logging info on video_to_landmarks

--- video_to_landmarks.py
+++ video_to_landmarks.py
@@ -47,7 +47,7 @@ class VideoToLandmarks:
                 if self.check_if_video_already_exists(video_name):
                     self.df_videos_infos = self.df_videos_infos.append({'video_name' : video_name, 'fps' : cap.get(cv2.CAP_PROP_FPS)}, ignore_index=True)
         else:
-            print("loading video : " + self.path.split("/")[-1])
+            logging.info("loading video : " + self.path.split("/")[-1])
             cap = cv2.VideoCapture(os.path.join(self.path))
             video_name = parse_path_to_name(self.path)
             self.videos.append({
@@ -63,7 +63,7 @@ class VideoToLandmarks:
 
     def transoform_videos_to_landmarks(self, face_recognition_type):
         for video in self.videos:
-            print("Writing video : " + str(video.get("video_name")))
+            logging.info("Writing video : " + str(video.get("video_name")))
             csv_path_name = "data/data_out/"+video.get("video_name")+".csv"
             success, image = video.get("video").read()
             count = 0;
