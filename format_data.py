891d79b30641fb095053079cf12c7decdb0932cd
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2021-01-20 20:40:16 +0100

    finish merge fusion of csv

--- format_data.py
+++ format_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from utils import paths_to_df
+
 class DataFormator:   
     def __init__(self):
         self.df_csv_files = {}
@@ -9,7 +10,7 @@ class DataFormator:
         self.face_recognitions_type = ["cnn", "hog"]
         self.data_folder = "data/data_out/"
         
-    def load_csv(self, video_name):
+    def load_csv_by_face_recognitions(self, video_name):
         for face_type in self.face_recognitions_type:
             self.df_csv_files.update({
                 face_type : pd.read_csv(self.data_folder+video_name+"_"+face_type+".csv").rename(columns={ "Unnamed: 0" : "frame"})
@@ -37,28 +38,37 @@ class DataFormator:
                         else: 
                             print(self.df_formatted.loc[self.df_formatted["frame"] == frame, self.df_formatted.columns != "frame"] + value.loc[value["frame"] == frame, value.columns != "frame"]/2)
             self.df_formatted.sort_values(by="frame")
-            
+    
+    def is_df_has_empty_frame(self, df, frame):
+        index_array = df.index.values
+        if frame in index_array: return False
+        else: return True
+    
+    ## TODO: Make frame index of all the DF by default
+    
     def merge_csv(self, csv_array_path):
         count_divide_dict = {}
         df_array = paths_to_df(csv_array_path)
-        self.df_merge = df_array[0].rename(columns={"Unnamed: 0" : "frame"})
-        #self.df_merge.set_index("frame")
+        self.df_merge = df_array[0].set_index("frame")
         for df in df_array[1:]:
-            for frame in df["frame"]:
-                if self.df_merge[self.df_merge["frame"] == frame].empty:
-                    self.df_merge =  self.df_merge.append(df[df["frame"] == frame])
-                else: 
-                    #print(self.df_merge.loc[self.df_merge["frame"] == frame, self.df_merge.columns != "frame"] + df.loc[df["frame"] == frame, df.columns != "frame"])
-                if count_divide_dict.get(frame) == None :
-                    #count_divide_dict.update({frame, 1})
-                    count_divide_dict[frame] = 1
-                else :
-                    count_divide_dict.update({frame, count_divide_dict.get(frame)+1})
-                print(count_divide_dict)
-                self.df_merge.sort_values(by="frame")
+            df = df.set_index("frame")     
+            for frame, landmarks in df.iterrows():
+                if self.is_df_has_empty_frame(self.df_merge, frame):
+                    if count_divide_dict.get(frame) == None :
+                        count_divide_dict[frame] = 1
+                    self.df_merge = self.df_merge.append(landmarks)
+                else:   
+                    if count_divide_dict.get(frame) == None :
+                        count_divide_dict[frame] = 1
+                    self.df_merge.loc[frame] =  self.df_merge.loc[frame] + landmarks              
+                    count_divide_dict.update({frame : count_divide_dict.get(frame)+1})
+        self.df_merge = self.df_merge.sort_index()
         for frame, divider in count_divide_dict.items():
-            self.df_merge[self.df_merge["frame"] == frame] / divider
-       
+            print("frame : ", frame)
+            print("divider : ", divider)
+            self.df_merge.loc[frame] = self.df_merge.loc[frame].divide(divider)
+        self.df_merge.to_csv("data/data_out/df_merge_test.csv")
+    
         
 df = DataFormator()
 csv_array = ["data/data_out/DESFAM_Semaine 2-Vendredi_Go-NoGo_H64_hog.csv", "data/data_out/DESFAM_Semaine 2-Vendredi_Go-NoGo_H69_hog.csv"]
