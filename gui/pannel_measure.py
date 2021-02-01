4e5fff927c6294aca9751417187ab8796366e1e0
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2021-02-01 12:53:27 +0100

    add genreci function to show measure list

--- gui/pannel_measure.py
+++ gui/pannel_measure.py
@@ -23,14 +23,15 @@ class MeasurePannel(tk.Frame):
         self.pannel_title.set("Measure")
         self.pannel_label = tk.Label(self, textvariable=self.pannel_title, bd=5, height=2, width=18)
         self.pannel_label.pack(side="top", pady=(10,0), padx=(10,10))
-        self.validate_button = TK.Button(self,)
-        self.entries = []
+        self.validate_button = tk.Button(self, text="Meeasure", bd=5, height=2, width=18, command= lambda: self.plot_info())
+        self.validate_button.pack(side=tk.BOTTOM)
+        self.check_buttons_measure_state = []
         self.add_measure("test")
         self.add_measure("test")
-        self.add_measure_input("test","tets_1","test_2")
-        self.add_measure_input("test","tets_1")
-
-    
+        self.add_measure("test","tets_1","test_2")
+        self.add_measure("test","tets_1")
+        
+    """
     def add_measure(self, measure_name):
         item_frame = tk.Frame(self, width=300, height=50, bg="yellow")
         item_text = tk.StringVar()
@@ -40,9 +41,10 @@ class MeasurePannel(tk.Frame):
         item_label.pack(pady=(12,0))
         item_frame.pack(pady=(10,0))
         item_frame.pack_propagate(0)
-
+        self.check_buttons_measure_state.append({"measure" : measure_name, "state" : state})
+"""
     
-    def add_measure_input(self, measure_name, input_name_1, input_name_2 = None):
+    def add_measure(self, measure_name, input_name_1 = None, input_name_2 = None):
         
         item_frame = tk.Frame(self, width=300, height=50, bg="yellow")
         item_title = tk.StringVar()
@@ -50,13 +52,14 @@ class MeasurePannel(tk.Frame):
         state = tk.IntVar()
         item_label = tk.Checkbutton(item_frame, variable= state, textvariable=item_title)
         
-        item_input = tk.StringVar()
-        item_input.set(input_name_1 + " : ")
-        label_input = tk.Label(item_frame, textvariable=item_input)
-        
-        ent = tk.Entry(item_frame, width = 5)    
-        ent.insert(0, "0")
-        self.entries.append(ent)
+        if(input_name_1 != None):
+            item_input = tk.StringVar()
+            item_input.set(input_name_1 + " : ")
+            label_input_1 = tk.Label(item_frame, textvariable=item_input)
+            
+            ent_1 = tk.Entry(item_frame, width = 5)    
+            ent_1.insert(0, "0")
+            self.check_buttons_measure_state.append({"measure" : measure_name, "state" : state, "input_1": input_name_1, "input_2" : input_name_2, "ent_1" :ent_1})
         
         if(input_name_2 != None):
             item_input_2 = tk.StringVar()
@@ -65,19 +68,25 @@ class MeasurePannel(tk.Frame):
             
             ent_2 = tk.Entry(item_frame, width = 5)    
             ent_2.insert(0, "0")
-            self.entries.append(ent_2)
-            
-        
+            self.check_buttons_measure_state.append({"measure" : measure_name, "state" : state, "input_1": input_name_1, "input_2" : input_name_2, "ent_1" :ent_1, "ent_2":ent_2})
 
-     
-    
         item_label.pack(side="top")
-        label_input.pack(side="left", padx=(20,0))
-        ent.pack(side="left", padx=(20,0))
+        if(input_name_1 != None):
+            label_input_1.pack(side="left", padx=(20,0))
+            ent_1.pack(side="left", padx=(20,0))
         if(input_name_2 != None):
             label_input_2.pack(side="left", padx=(20,0))
             ent_2.pack(side="left", padx=(20,0))
         item_frame.pack(pady=(10,0))
         item_frame.pack_propagate(0)
+        
+        
+    def plot_info(self):
+        for measure in self.check_buttons_measure_state:
+            
+            print(measure.get("measure"))
+            print(measure.get("state").get())
+            if measure.get("ent_1") != None :
+                print(measure.get("ent_1").get())
 
         
