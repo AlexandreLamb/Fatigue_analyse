d652f792692cb8e517eed65a9029d2eeee2c2087
valoulabest <valentine.mace@hotmail.com>
2021-02-02 13:58:28 +0100

    stylisation frame mesures

--- gui/pannel_measure.py
+++ gui/pannel_measure.py
@@ -14,42 +14,62 @@ from utils import parse_path_to_name
 import random
 class MeasurePannel(tk.Frame):
     def __init__(self, parent, video_name):
-        tk.Frame.__init__(self, parent, width=400, height=700, bg='gray80')
-        tk.Frame.pack(self, side="right")
+        tk.Frame.__init__(self, parent, width=450, height=700, highlightbackground="black", highlightthickness=3)
+        tk.Frame.pack(self, side="right", padx=(50,0))
         tk.Frame.pack_propagate(self,0)
-         
+
         self.pannel_title = tk.StringVar()
-        self.pannel_title.set("Measure on \n"+video_name)
+        self.pannel_title.set("Measure on the following video: \n"+video_name)
         self.pannel_label = tk.Label(self, textvariable=self.pannel_title, bd=5, height=4, width=50)
-        self.pannel_label.pack(side="top", pady=(10,0), padx=(10,10))
-       
-        self.contenaire_measure = tk.Canvas(self, width=400, height=400,scrollregion=(0,0,0,300))
-        self.scrollbar_measure = tk.Scrollbar(self.contenaire_measure, orient=tk.VERTICAL)
+        self.pannel_label.pack(side="top", pady=(3,0), padx=(10,10))
+
+        #frame for measures
+        self.frame_measures = tk.Frame(self, width=450, height=600)
+        self.frame_measures.pack()
+         
+        self.contenaire_measure = tk.Canvas(self.frame_measures, width=450, height=510,scrollregion=(0,0,0,800))
+        
+        self.scrollbar_measure = tk.Scrollbar(self.frame_measures, orient=tk.VERTICAL)
         self.scrollbar_measure.pack(side=tk.RIGHT, fill= Y)
-        self.contenaire_measure.pack()
-        self.contenaire_measure.config(yscrollcommand=self.scrollbar_measure.set)
         self.scrollbar_measure.config(command=self.contenaire_measure.yview)
         
-        self.validate_button = tk.Button( self.contenaire_measure, text="Compute", bd=5, height=2, width=18, command = lambda: self.event_generate("<<COMPUTE>>"))
-        self.validate_button.pack(side=tk.BOTTOM)
+        # self.scrollbar_measure2 = tk.Scrollbar(self.contenaire_measure, orient=tk.HORIZONTAL)
+        # self.scrollbar_measure2.pack(side=tk.BOTTOM, fill= X)
+        # self.contenaire_measure.pack()
+    
+        self.contenaire_measure.config(yscrollcommand=self.scrollbar_measure.set)
+        self.contenaire_measure.pack()
+        
+        #self.scrollbar_measure2.config(command=self.contenaire_measure.xview)
+
+        self.frame_button = tk.Frame(self, width=450, height=200)
+        self.frame_button.pack(pady=(10,0))
+
+        self.contenaire_measure2 = tk.Canvas(self.frame_button, width=450, height=200, bg='blue')
+        self.contenaire_measure2.pack()
+
+        self.validate_button = tk.Button(self.contenaire_measure2, text="Compute", height=3, width=18, command = lambda: self.event_generate("<<COMPUTE>>"))
+        self.validate_button.pack()
         self.check_buttons_measure_state = []
-       
-        self.add_measure("yawning_frequency", "threshold (sec)")
-        self.add_measure("blinking_frequency", "threshold (sec)")
+
+        self.add_measure("yawning_frequency", "\ threshold (sec)")
+        self.add_measure("blinking_frequency", "\ threshold (sec)")
         self.add_measure("ear")
-        self.add_measure("perclos_measure","threshold (sec)", "percentage")
-        self.add_measure("microsleep_measure","threshold (sec)")
+        self.add_measure("perclos_measure","\ threshold (sec)", "\ percentage")
+        self.add_measure("microsleep_measure","\ threshold (sec)")
         self.add_measure("eyebrow_nose")
         self.add_measure("eyebrow_eye")
         self.add_measure("eye_area")
-        self.add_measure("eye_area_mean_over_","threshold (sec)")
+        self.add_measure("eye_area_mean_over","\ threshold (sec)")
         self.add_measure("eye_angle")
+
 
     def update_title(self, video_name):
          self.pannel_title.set("Measure on \n"+video_name)
+
     def add_measure(self, measure_name, input_name_1 = None, input_name_2 = None):
         
-        item_frame = tk.Frame(self.contenaire_measure, width=350, height=50, bg="yellow")
+        item_frame = tk.Frame(self.contenaire_measure, width=350, height=45)
         item_title = tk.StringVar()
         item_title.set(measure_name)
         state = tk.IntVar()
@@ -85,20 +105,20 @@ class MeasurePannel(tk.Frame):
             self.check_buttons_measure_state.append({"measure" : measure_name, "state" : state, "input_1": input_name_1, "input_2" : input_name_2, "ent_1" :ent_1, "ent_2":ent_2})
        
         if((input_name_1 == None) and (input_name_2 == None)):
-            item_label.pack(side="top", pady=(12,0))
+            item_label.pack(side="left", pady=(0,0))
         else:
-            item_label.pack(side="top")
+            item_label.pack(side="left", pady=(0,0))
             
         if((input_name_1 != None) and (input_name_2 == None)):
-            label_input_1.pack(side="left", padx=(20,0))
-            ent_1.pack(side="left", padx=(20,0))
+            label_input_1.pack(side="left", pady=(2,0), padx=(0,0))
+            ent_1.pack(side="left", pady=(5,0), padx=(0,0))
         if((input_name_1 != None) and (input_name_2 != None)):
-            label_input_1.pack(side="left", padx=(20,0))
-            ent_1.pack(side="left", padx=(20,0))
-            label_input_2.pack(side="left", padx=(20,0))
-            ent_2.pack(side="left", padx=(20,0))
+            label_input_1.pack(side="left", pady=(2,0), padx=(0,0))
+            ent_1.pack(side="left", pady=(5,0), padx=(0,0))
+            label_input_2.pack(side="left", pady=(2,0), padx=(0,0))
+            ent_2.pack(side="left", pady=(5,0), padx=(0,0))
         
-        item_frame.pack(pady=(10,0))
+        item_frame.pack(pady=(5,0))
         item_frame.pack_propagate(0)
         
         
