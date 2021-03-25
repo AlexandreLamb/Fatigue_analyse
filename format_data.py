e15f16d62a10bd89b4b56dbec73b47cddaed9e51
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2021-03-25 17:28:11 +0100

    make dataset in notebook

--- format_data.py
+++ format_data.py
@@ -3,12 +3,15 @@ import numpy as np
 from utils import paths_to_df, parse_path_to_name
 from datetime import datetime
 class DataFormator:   
+    VIDEOS_INFOS_PATH = "data/stage_data_out/videos_infos.csv"
+
     def __init__(self):
         self.df_csv_files = {}
         self.df_formatted = None
         self.df_merge = None
         self.face_recognitions_type = ["cnn", "hog"]
         self.data_folder = "data/data_out/"
+        #self.video_info_path("data/stage_data_out/videos_infos.csv")
         
     def load_csv_by_face_recognitions(self, video_name):
         for face_type in self.face_recognitions_type:
@@ -77,5 +80,25 @@ class DataFormator:
                                                                     ignore_index=True)
         df_videos_merge_infos.to_csv("data/data_out/videos_infos.csv", mode="a", header=False)
         return csv_path.split(".")[0]
-    
+    @staticmethod
+    def make_label_df(num_min, video_name, df_measure= pd.DataFrame(), path = None):
+        df_video_infos = pd.read_csv(DataFormator.VIDEOS_INFOS_PATH)
+        fps = list(df_video_infos[df_video_infos["video_name"] == video_name]["fps"])[0]
+        num_sec = num_min*60
+        num_frame_by_num_min = int(fps*num_sec)
+        if path != None:
+            df = pd.read_csv(path)
+        if not df_measure.empty:
+            df = df_measure
+            df_label = df.append( pd.DataFrame(columns=['Target']))
+
+            df_label.loc[lambda df_label: df_label["frame"] <= num_frame_by_num_min,"Target"] = 0
+
+            df_label.loc[lambda df_label: df_label["frame"] > num_frame_by_num_min,"Target"] = 1
+            
+        return df_label
+
+
 ## TODO:  add video anme and stuff in csv video infos
+
+## TODO: make mother class for herite some commun variable (csv_infos ect...)
