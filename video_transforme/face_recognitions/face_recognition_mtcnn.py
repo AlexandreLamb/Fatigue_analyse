bc8d3941a19f64d0d9e6e218c0d31d5c0b209ecb
alexandreLamb <alexandre.lambert@edu.ece.fr>
2021-04-07 10:26:26 +0200

    add hog detection on mtcnn if no face detect

--- video_transforme/face_recognitions/face_recognition_mtcnn.py
+++ video_transforme/face_recognitions/face_recognition_mtcnn.py
@@ -3,6 +3,8 @@ import cv2
 import dlib
 import pandas as pd
 from imutils import face_utils
+from face_recognitions import FaceRecognitionHOG
+
 
 SHAPE_PREDICTOR_PATH ="data/data_in/models/shape_predictor_68_face_landmarks.dat"
 
@@ -14,9 +16,21 @@ class FaceRecognitionMtcnn:
     def place_landmarks(self, img, count):
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         face_informations = self.detector.detect_faces(img_gray)
-        box = face_informations[0].get("box")
-        rectangle_box = dlib.rectangle(box[0]+box[2], box[1], box[0], box[1]+box[3])
-        marks = self.predictor(img_gray, rectangle_box)
-        marks = face_utils.shape_to_np(marks)
+        if len(face_informations) == 1:
+            logging.info("Detect 1 face on img number " +str(count))
+            box = face_informations[0].get("box")
+            rectangle_box = dlib.rectangle(box[0]+box[2], box[1], box[0], box[1]+box[3])
+            marks = self.predictor(img_gray, rectangle_box)
+            marks = face_utils.shape_to_np(marks)
+        elif len(face_informations) > 1:
+            logging.info("Detect more than 1 face on img number " +str(count) + " get default first face detect")
+        
+        else :
+            logging.info("No face on img number " +str(count) + " try with HOG classifier")
+            hog_recogniton = FaceRecognitionHOG()
+            marks = hog_recogniton.place_landmarks_from_mtcnn(img,count)
+            if len(marks) == 0:
+                logging.info("No face on img number " +str(count)+ " even with HOG")
+
         return marks.ravel()
            
