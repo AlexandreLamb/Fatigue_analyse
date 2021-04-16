3c280a699d45bb1b2a9fa6f16f2edb48036abb2d
alexandreLamb <alexandre.lambert@edu.ece.fr>
2021-04-16 11:15:54 +0200

    change import

--- test.py
+++ test.py
@@ -1,23 +1,17 @@
 import argparse, sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from data_manipulation import AnalyseData
-from data_manipulation import DataFormator
+
+from format_data import DataFormator
 import json
 import os
 
-parser=argparse.ArgumentParser()
-
-
-parser.add_argument('--path', help='path to the landamrks folder')
-#parser.add_argument('--json_measure', help='path to measure json file')
-
-args=parser.parse_args()
-
+folder_path = "data/stage_data_out_all_landamrks/landmarks_csv/mtcnn"
 #json_measure = json.load(args.json_measure)
 
-csv_array_name  = os.listdir(args.path)
+csv_array_name  = os.listdir(folder_path)
 
-csv_array_path = [args.path + name for name in csv_array_name]
+csv_array_path = [folder_path + name for name in csv_array_name]
 
 df_ear_all = pd.DataFrame()
 
@@ -27,7 +21,7 @@ for index, csv_landmarks_path in enumerate(csv_array_path) :
     analyse_data = AnalyseData(csv_landmarks_path)
     #TODO: make a function who take a json file of meaurec
     analyse_data.measure_ear()
-
+    print(analyse_data.df_measure)
     df_ear = DataFormator.make_label_df(num_min = 5, video_name = video_name, df_measure= analyse_data.df_measure)
     df_ear_all = df_ear_all.append(df_ear)
     DataFormator.save_df(df_ear, video_name)
