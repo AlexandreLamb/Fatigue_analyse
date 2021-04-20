a54ba6b4d1f90be4a437d0952727fdf998f9f183
alexandreLamb <alexandre.lambert@edu.ece.fr>
2021-04-20 11:01:22 +0200

    add atribut in init of preprocessing

--- fatigue_model/data_processing/data_preprocessing.py
+++ fatigue_model/data_processing/data_preprocessing.py
@@ -7,13 +7,19 @@ from tensorflow.keras.layers.experimental import preprocessing
 
 class DataPreprocessing()
     def __init__(self, batch_size, path_to_dataset):
-        self.all_inputs = []
-        self.encoded_features = []
-        self.batch_size = batch_size
-        self.all_features = []
         self.path_to_dataset = path_to_dataset
         self.dataset = None
+        self.train = None
+        self.test = None
+        self.val = None
+        
+        self.all_features = []
+        self.all_inputs = []
+        self.encoded_features = []
         self.numerical_column = None
+        
+        self.batch_size = batch_size
+        
         self.initialize()
     
     def make_numerical_feature_col(self, normalize = False):    
@@ -45,28 +51,28 @@ class DataPreprocessing()
         val_size = int(0.15*dataset_size)
         test_size = int(0.15*dataset_size)
 
-        train = dataset.take(train_size)
-        val = dataset.skip(train_size)
-        val = dataset.take(val_size)
-        test = dataset.skip(train_size + val_size)
-        test = dataset.take(test_size)
+        self.train = dataset.take(train_size)
+        self.val = dataset.skip(train_size)
+        self.val = dataset.take(val_size)
+        self.test = dataset.skip(train_size + val_size)
+        self.test = dataset.take(test_size)
 
-        train_size = train.reduce(0, lambda x, _: x + 1).numpy()
-        val_size = val.reduce(0, lambda x, _: x + 1).numpy()
-        test_size = test.reduce(0, lambda x, _: x + 1).numpy()
+        train_size = self.train.reduce(0, lambda x, _: x + 1).numpy()
+        val_size = self.val.reduce(0, lambda x, _: x + 1).numpy()
+        test_size = self.test.reduce(0, lambda x, _: x + 1).numpy()
 
         print("Full dataset size:", dataset_size)
         print("Train dataset size:", train_size)
         print("Val dataset size:", val_size)
         print("Test dataset size:", test_size)
 
-        train = train.shuffle(buffer_size = train_size)
-        train = train.batch(self.batch_size)
+        self.train = self.train.shuffle(buffer_size = train_size)
+        self.train = self.train.batch(self.batch_size)
 
-        val = val.shuffle(buffer_size = val_size)
-        val = val.batch(self.batch_size)
+        self.val = self.val.shuffle(buffer_size = val_size)
+        self.val = self.val.batch(self.batch_size)
 
-        test = test.batch(self.batch_size)
+        self.test = self.test.batch(self.batch_size)
         
         def load_dataset(self):                  
             df = pd.read_csv(self.path_to_dataset, index_col=0)
