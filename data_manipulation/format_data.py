a2b28c818284a77995e9581d2969c65117d9dde3
AlexandreLamb <alexandre.lambert@edu.ece.fr>
2021-05-05 16:32:23 +0200

    add print of df in lmerge dataset

--- data_manipulation/format_data.py
+++ data_manipulation/format_data.py
@@ -197,6 +197,7 @@ class DataFormator:
         path_csv_arr = [path_to_measure_folder+"/"+ dir_name+"/"+dir_name+".csv" for dir_name in dir_measures]
         df_measures = pd.DataFrame()
         for path in path_csv_arr:
+            print(pd.read_csv(path, index_col=0)["target"].sum())
             df_measures = df_measures.append(pd.read_csv(path, index_col=0), ignore_index=True)
         df_measures.to_csv("data/stage_data_out/dataset/Merge_Dataset/dataset_merge_"+str(windows)+".csv")
         
