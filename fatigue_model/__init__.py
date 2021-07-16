f89f5b1d3aae48d9ac2734c163329f92b0852c20
alexandreLamb <alexandre.lambert@edu.ece.fr>
2021-07-16 12:42:05 +0200

    add class for cross valdiation

--- fatigue_model/__init__.py
+++ fatigue_model/__init__.py
@@ -1,4 +1,4 @@
 from .evaluate_model import evaluate
 from .train_ann_dense import train_dense
-from .train_temporal_cross import train_evaluate_model
 from .train_ann_temoral import train_temporal
+from .train_temporal_cross import CrossValidation
---
+++ script/generate_cross_validation_results.py
@@ -0,0 +1,14 @@
+import os, sys
+from dotenv import load_dotenv
+
+load_dotenv("env_file/.env_path")
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from fatigue_model import CrossValidation
+
+PATH_TO_TIME_ON_TASK_CROSS = os.environ.get("PATH_TO_TIME_ON_TASK_CROSS")
+
+cross_validator = CrossValidation(PATH_TO_TIME_ON_TASK_CROSS)
+cross_validator.train_cross_model()
+
+del cross_validator
