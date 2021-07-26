508e252006e3b9646817f7324f10af5853b99f0b
AlexandreLamb <alexandre.lambert@edu.ece>
2021-07-26 10:06:43 +0200

    delete print

--- script/generate_cross_validation_results_debt.py
+++ script/generate_cross_validation_results_debt.py
@@ -7,7 +7,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from fatigue_model import CrossValidation
 
 PATH_TO_DEBT_CROSS = os.environ.get("PATH_TO_DEBT_CROSS")
-print(PATH_TO_DEBT_CROSS)
 
 cross_validator = CrossValidation(PATH_TO_DEBT_CROSS)
 cross_validator.train_cross_model()
